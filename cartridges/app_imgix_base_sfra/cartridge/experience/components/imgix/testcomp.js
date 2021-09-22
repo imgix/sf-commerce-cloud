57d653b363f4a6cadc0c5a815937f86106be6155
Luis Ball <lball@imgix.com>
2021-09-22 15:26:26 -0700

    test: add debugging logging to visualize data flow
    This commit attempts to add some logging calls to see exactly how the
    data is flowing from the json to the js to the isml. The blocker here is
    that only some of the `context.content` attributes are actually making
    it through. You can see that `${pdict.title}` for example always
    evaluates to null, even if we pass in a `title` string in the page
    designer.

--- cartridges/app_imgix_base_sfra/cartridge/experience/components/imgix/testcomp.js
+++ cartridges/app_imgix_base_sfra/cartridge/experience/components/imgix/testcomp.js
@@ -1,21 +1,30 @@
-'use strict';
+"use strict";
 // These all are SFCC APIs
-var Template = require('dw/util/Template');
-var HashMap = require('dw/util/HashMap');
+var Template = require("dw/util/Template");
+var HashMap = require("dw/util/HashMap");
 // we included an existing SFCC back-end script to handle the image path
-var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+var ImageTransformation = require("*/cartridge/experience/utilities/ImageTransformation.js");
+// Logging class used for debugging
+var log = require("dw/system").Logger.getLogger("imgix", "");
 
 // handle function for component.
 module.exports.render = function (context, modelIn) {
-    var model = modelIn || new HashMap();
-    var content = context.content;
+  var model = modelIn || new HashMap();
+  var content = context.content;
 
-    model.title = content.title ? content.title : null;
-    model.image = ImageTransformation.getScaledImage(content.image);
-    // use to give link on the image, if we click on image it take to us to that page.
-    model.link = content.ITCLink ? content.imageLink : '#';
-    model.alt = content.alt ? content.alt : null;
+  // let contentStr = `${content.title}:${content.image}:${content.link}:${content.alt}`;
 
-    
-    return new Template('/imgix/image/imgerender/imagerender').render(model).text;
+  model.title = content.title ? content.title : null;
+  model.image = ImageTransformation.getScaledImage(content.image);
+  // use to give link on the image, if we click on image it take to us to that page.
+  model.link = content.ITCLink ? content.imageLink : "#";
+  model.alt = content.alt ? content.alt : null;
+
+  // log.warn("content: {0}", contentStr);
+  // log.warn("model title: {0}", model.title);
+  // log.warn("model image: {0}", model.image);
+  // log.warn("model link: {0}", model.link);
+  // log.warn("model alt: {0}", model.alt);
+
+  return new Template("/imgix/image/imgerender/imagerender").render(model).text;
 };
