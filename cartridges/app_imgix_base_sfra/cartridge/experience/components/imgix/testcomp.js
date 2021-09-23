a7852bb9f2c79f937d99c75e0c885e97c9542092
Luis Ball <lball@imgix.com>
2021-09-23 08:41:17 -0700

    chore: fix spelling and spacing
    This commit fixes the spelling of `title` to `tittle` to match the
    spelling in the `.json` for this component. It also fixes the new line
    spacing to match our linting rules.

--- cartridges/app_imgix_base_sfra/cartridge/experience/components/imgix/testcomp.js
+++ cartridges/app_imgix_base_sfra/cartridge/experience/components/imgix/testcomp.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 // These all are SFCC APIs
-var Template = require('dw/util/Template');
-var HashMap = require('dw/util/HashMap');
+var Template = require("dw/util/Template");
+var HashMap = require("dw/util/HashMap");
 // we included an existing SFCC back-end script to handle the image path
-var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+var ImageTransformation = require("*/cartridge/experience/utilities/ImageTransformation.js");
 
 // handle function for component.
 module.exports.render = function (context, modelIn) {
-    var model = modelIn || new HashMap();
-    var content = context.content;
+  var model = modelIn || new HashMap();
+  var content = context.content;
 
-    model.title = content.title ? content.title : null;
-    model.image = ImageTransformation.getScaledImage(content.image);
-    // use to give link on the image, if we click on image it take to us to that page.
-    model.link = content.ITCLink ? content.imageLink : '#';
-    model.alt = content.alt ? content.alt : null;
+  model.title = content.title ? content.tittle : null;
+  model.image = ImageTransformation.getScaledImage(content.image);
+  // use to give link on the image, if we click on image it take to us to that page.
+  model.link = content.imageLink ? content.imageLink : "#";
+  model.alt = content.alt ? content.alt : null;
 
-    
-    return new Template('/imgix/image/imgerender/imagerender').render(model).text;
+  return new Template("/imgix/image/imgerender/imagerender").render(model).text;
 };
