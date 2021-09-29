58b25e26f0506a659dfa11151a65c76724573893
Luis Ball <lball@imgix.com>
2021-09-29 10:28:19 -0700

    feat: add basic frontend structure

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello world</p>
+      </header>
+    </div>
+  );
+}
