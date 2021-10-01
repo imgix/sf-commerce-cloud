ff5ed27a42b3eda9d5026c48dd14d50239ace08c
Luis Ball <lball@imgix.com>
2021-10-01 11:47:03 -0700

    feat: add basic searchForm

---
+++ frontend/src/components/forms/search/SearchBar.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from "react";
+import { SearchIconSvg } from "../../icons/SearchIconSvg";
+import "../../../styles/Form.css";
+
+interface Props {
+  placeholder?: string;
+}
+
+export function SearchBar({ placeholder }: Props): ReactElement {
+  return (
+    <form className="ix-asset-simple-search-content">
+      <div className="ix-asset-simple-search-wrapper">
+        <div className="ix-asset-simple-search-base">
+          <div className="ix-asset-simple-search-base-input">
+            <div className="ix-asset-simple-search-icon">
+              <SearchIconSvg />
+            </div>
+            <input
+              type="text"
+              className="ix-asset-simple-search-base-input-field"
+              placeholder={
+                placeholder ? placeholder : "Search by filename or path"
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
