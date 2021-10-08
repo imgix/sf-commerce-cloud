635997fe3e20d3ea373d82761f5a4c32b671345f
Luis Ball <lball@imgix.com>
2021-10-08 13:34:23 -0700

    chore: add asset data type
    This commit changes the `ImgixGETImagesData` to `ImgixGETAssetsData`.
    This is done in order to account retrieving assets other than images in
    the future with this component.

--- frontend/src/types/imgixAPITypes.ts
+++ frontend/src/types/imgixAPITypes.ts
@@ -6,10 +6,40 @@ export type ImgixGETSourcesData = {
   type: string;
 }[];
 
-export type ImgixGETImagesData = {
+export type ImgixGETAssetsData = {
   attributes: {
-    name: string;
+    analyzed_content_warnings: boolean;
+    analyzed_faces: boolean;
+    analyzed_tags: boolean;
+    categories: null | [];
+    color_model: string;
+    color_profile: string;
+    colors: {};
+    content_type: string;
+    custom_fields: null | string | string[];
+    date_created: number;
+    date_modified: number;
+    description: null | string;
+    dpi_height: number;
+    dpi_width: number;
+    face_count: null | string | number;
+    file_size: number;
+    has_frames: boolean;
+    media_height: number;
+    media_kind: null | string;
+    media_width: number;
+    name: null | string | string[];
+    origin_path: string;
+    source_id: string;
+    tags: {};
+    uploaded_by: null | string | string[];
+    uploaded_by_api: boolean;
+    warning_adult: number;
+    warning_medical: number;
+    warning_racy: number;
+    warning_spoof: number;
+    warning_violence: number;
   };
   id: string;
-  type: string;
+  type: "assets";
 }[];
