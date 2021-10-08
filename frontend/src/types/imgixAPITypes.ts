2a48fedbc5c736724d5861adfe40fa7beeccac15
Luis H. Ball Jr <luqven@gmail.com>
2021-10-08 15:56:37 -0700

    chore: remove fields outside of current scope
    Removing fields that are not yet needed in the application.

--- frontend/src/types/imgixAPITypes.ts
+++ frontend/src/types/imgixAPITypes.ts
@@ -8,37 +8,10 @@ export type ImgixGETSourcesData = {
 
 export type ImgixGETAssetsData = {
   attributes: {
-    analyzed_content_warnings: boolean;
-    analyzed_faces: boolean;
-    analyzed_tags: boolean;
-    categories: null | [];
-    color_model: string;
-    color_profile: string;
-    colors: {};
-    content_type: string;
-    custom_fields: null | string | string[];
-    date_created: number;
-    date_modified: number;
     description: null | string;
-    dpi_height: number;
-    dpi_width: number;
-    face_count: null | string | number;
-    file_size: number;
-    has_frames: boolean;
-    media_height: number;
-    media_kind: null | string;
-    media_width: number;
-    name: null | string | string[];
+    name: null | string;
     origin_path: string;
     source_id: string;
-    tags: {};
-    uploaded_by: null | string | string[];
-    uploaded_by_api: boolean;
-    warning_adult: number;
-    warning_medical: number;
-    warning_racy: number;
-    warning_spoof: number;
-    warning_violence: number;
   };
   id: string;
   type: "assets";
