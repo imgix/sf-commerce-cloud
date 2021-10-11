e1f1dd641a55333dc101fb21ba693045dcae592e
Luis Ball <lball@imgix.com>
2021-10-11 12:11:23 -0700

    feat: add api requests and split component
    This commit add imgix api data fetching and splits the asset browser
    into mutiple components. This allows us to have an `AssetGridContainer`
    that can conditionally render a placeholder or the asset grid.

--- frontend/src/components/AssetBrowser/AssetBrowser.tsx
+++ frontend/src/components/AssetBrowser/AssetBrowser.tsx
@@ -1,8 +1,9 @@
 import React, { ReactElement } from "react";
-import { AssetGrid } from "../grids/AssetGrid";
-import { SourceSelect } from "../buttons/dropdowns/SourceSelect";
-import { SearchBar } from "../forms/search/SearchBar";
 
+import { AssetGridContainer } from "./AssetGridContainer";
+import { imgixAPI } from "../../services/imgixAPIService";
+import { SearchBar } from "../forms/search/SearchBar";
+import { SourceSelect } from "../buttons/dropdowns/SourceSelect";
 import { SourceT } from "../../types";
 
 import "../../styles/AssetBrowser.css";
@@ -13,17 +14,52 @@ interface Props {
 
 export function AssetBrowser({ apiKey }: Props): ReactElement {
   const [sources, setSources] = React.useState<SourceT[]>([]);
+  const [selectedSource, setSelectedSource] = React.useState<SourceT>();
   const [assets, setAssets] = React.useState<any[]>([]);
+
+  const handleSourceSelect = (sourceId: string) => {
+    // request images from that source
+    const source = sources.find(
+      (currentSource) => currentSource.id === sourceId
+    );
+    if (!source) return;
+    setSelectedSource(source);
+
+    console.log(apiKey, source);
+
+    imgixAPI.sources.assets
+      .get(apiKey, source.id)
+      .then((res) => {
+        setAssets(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  React.useEffect(() => {
+    imgixAPI.sources.get(apiKey).then((resp) => {
+      setSources(resp.data);
+    });
+  }, [apiKey]);
+
+  const parseSourceDomain = (source: SourceT) => {
+    if (!source) return "";
+    const customDomains = source?.attributes?.custom_domains;
+    if (!customDomains || !customDomains.length)
+      return source.attributes.name + ".imgix.net";
+  };
+
+  const domain = parseSourceDomain(selectedSource as SourceT);
+  console.log(domain);
 
   return (
     <div className="ix-asset-browser">
       <div className="ix-asset-title-bar-container">
-        <SourceSelect sources={sources} />
+        <SourceSelect sources={sources} handleSelect={handleSourceSelect} />
         <SearchBar />
       </div>
-      <div className="ix-asset-grid-container">
-        <AssetGrid assets={assets} />
-      </div>
+      <AssetGridContainer domain={domain || ""} assets={assets} />
       <div className="ix-asset-meta-information-container"></div>
     </div>
   );
