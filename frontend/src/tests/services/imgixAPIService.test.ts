f2b44745f90ef4dd6eba7ce8a38e5f9068b243eb
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-10-12 11:32:52 +0200

    test: basic assets test

--- frontend/src/tests/services/imgixAPIService.test.ts
+++ frontend/src/tests/services/imgixAPIService.test.ts
@@ -1,7 +1,10 @@
 import ImgixManagementJS, { APIError } from "imgix-management-js";
 import { mocked } from "ts-jest/utils";
 import { imgixAPI } from "../../services/imgixAPIService";
-import { ImgixGETSourcesData } from "../../types/imgixAPITypes";
+import {
+  ImgixGETAssetsData,
+  ImgixGETSourcesData,
+} from "../../types/imgixAPITypes";
 
 jest.mock("imgix-management-js");
 
@@ -15,14 +18,25 @@ const testSource: ImgixGETSourcesData[0] = {
   type: "sources",
 };
 
+const testAsset: ImgixGETAssetsData[0] = {
+  attributes: {
+    description: "Test Asset Description",
+    name: "Test Asset",
+    origin_path: "path/to/image.jpg",
+  },
+  id: "2345",
+  type: "assets",
+};
+
 beforeEach(() => {
   ImgixManagementJSMock.mockClear();
-  const mockRequest = jest.fn().mockResolvedValue({
-    data: [testSource],
+  const mockRequest = jest.fn().mockImplementation(async (url) => ({
+    // Hacky way to mock the response from the API, change url matching in future
+    data: url.startsWith("sources?") ? [testSource] : [testAsset],
     included: [],
     jsonapi: {},
     meta: {},
-  });
+  }));
   ImgixManagementJSMock.mockImplementation(({ apiKey, version }) => {
     return {
       request: mockRequest,
@@ -36,4 +50,12 @@ beforeEach(() => {
 test("should return sources correctly", async () => {
   const data = await imgixAPI.sources.get("test-api-key");
   expect(data.data[0]).toMatchObject(testSource);
+});
+
+test("should return assets correctly", async () => {
+  const data = await imgixAPI.sources.assets.get(
+    "test-api-key",
+    "test1234abcd"
+  );
+  expect(data.data[0]).toMatchObject(testAsset);
 });
