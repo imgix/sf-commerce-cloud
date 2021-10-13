a51b582a886b8e2e86314784d63986711a64d2de
Luis Ball <lball@imgix.com>
2021-10-13 13:08:16 -0700

    chore: remove console logs

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,13 +3,10 @@ import { App as BmApp } from "./BmImgixPd";
 
 let App: any;
 
-console.log(process.env);
-
 if (process.env.REACT_APP_APP_TYPE === "int") {
   App = IntApp;
 } else if (process.env.REACT_APP_APP_TYPE === "bm") {
   App = BmApp;
-  console.log("Building Bm App...");
 } else {
   throw new Error("REACT_APP_APP_TYPE is not set");
 }
