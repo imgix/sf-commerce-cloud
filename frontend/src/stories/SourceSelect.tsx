93a88eb648d747521b85434230ba1a1f7a95443a
Luis Ball <lball@imgix.com>
2021-10-14 12:09:08 -0700

    chore(sources): use imgix api type in component

--- frontend/src/stories/SourceSelect.tsx
+++ frontend/src/stories/SourceSelect.tsx
@@ -1,16 +1,17 @@
 import React, { ReactElement } from "react";
 import { SourceSelect as _SourceSelect } from "../components/buttons/dropdowns/SourceSelect";
-import { SourceT } from "../types";
+import { ImgixGETSourcesData } from "../types";
 import "./ButtonLayout.css";
 
 interface Props {
-  sources: SourceT[];
+  sources: ImgixGETSourcesData;
 }
 
 export function SourceSelect({ sources }: Props): ReactElement {
+  const handleSelect = (source: string) => {};
   return (
     <div className="button-story-layout">
-      <_SourceSelect sources={sources} />
+      <_SourceSelect handleSelect={handleSelect} sources={sources} />
     </div>
   );
 }
