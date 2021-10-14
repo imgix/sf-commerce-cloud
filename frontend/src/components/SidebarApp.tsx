6b61969e9760cde5b3451b84b8e21ece40794ac2
Luis Ball <lball@imgix.com>
2021-10-14 12:44:06 -0700

    build: change react app type names
    This commit changes refences to `int` and `bm` to now be `sidebar` and
    `breakout`. This is because the bundles will likely both belong in the
    `int` cartridge and be differentiated by whether they're applied to the
    sidebar or the breakout editor.

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -4,7 +4,7 @@ export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>bm_imgix_pd_react_app</p>
+        <p>SideBarReactApp</p>
       </header>
     </div>
   );
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,12 +1,12 @@
-import { App as IntApp } from "./IntImgixPd";
-import { App as BmApp } from "./BmImgixPd";
+import { App as SidebarApp } from "./SidebarApp";
+import { App as BreakoutApp } from "./BreakoutApp";
 
 let App: any;
 
-if (process.env.REACT_APP_APP_TYPE === "int") {
-  App = IntApp;
-} else if (process.env.REACT_APP_APP_TYPE === "bm") {
-  App = BmApp;
+if (process.env.REACT_APP_APP_TYPE === "sidebar") {
+  App = SidebarApp;
+} else if (process.env.REACT_APP_APP_TYPE === "breakout") {
+  App = BreakoutApp;
 } else {
   throw new Error("REACT_APP_APP_TYPE is not set");
 }
