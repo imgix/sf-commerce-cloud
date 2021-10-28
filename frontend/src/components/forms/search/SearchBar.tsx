e7840090a9419b86261ae0bd5222098e3d948ff6
Luis Ball <lball@imgix.com>
2021-10-28 15:37:49 -0700

    chore: add on submit and change handlers
    This commit adds `onSubmit` and `onChange` handlers to the `SearchBar`
    component. This is necessary in order to change this form into a
    controlled form. Moreover, it makes integration of this component with
    custom handlers easier.

--- frontend/src/components/forms/search/SearchBar.tsx
+++ frontend/src/components/forms/search/SearchBar.tsx
@@ -4,11 +4,26 @@ import "../../../styles/Form.css";
 
 interface Props {
   placeholder?: string;
+  handleSubmit: (value: string) => void;
 }
 
-export function SearchBar({ placeholder }: Props): ReactElement {
+export function SearchBar({ placeholder, handleSubmit }: Props): ReactElement {
+  const [query, setQuery] = React.useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setQuery(e.currentTarget.value);
+  };
   return (
-    <form className="ix-asset-simple-search-content">
+    <form
+      className="ix-asset-simple-search-content"
+      onSubmit={(e) => {
+        // Prevent the form from submitting, i.e. reloading the page
+        e.preventDefault();
+        // Call the handleSubmit function that was passed through props
+        handleSubmit(query);
+      }}
+    >
       <div className="ix-asset-simple-search-wrapper">
         <div className="ix-asset-simple-search-base">
           <div className="ix-asset-simple-search-base-input">
@@ -21,6 +36,15 @@ export function SearchBar({ placeholder }: Props): ReactElement {
               placeholder={
                 placeholder ? placeholder : "Search filename or path"
               }
+              value={query}
+              onChange={(event) => {
+                event.preventDefault();
+                handleInputChange(event);
+              }}
+              onSubmit={(event) => {
+                event.preventDefault();
+                handleSubmit(query);
+              }}
             />
           </div>
         </div>
