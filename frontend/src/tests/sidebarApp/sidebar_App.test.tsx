10a12b164a7338a1dc15f6dcd9a65ffe3a1cf916
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-02 11:14:19 +0100

    test: fix sidebar integration test

--- frontend/src/tests/sidebarApp/sidebar_App.test.tsx
+++ frontend/src/tests/sidebarApp/sidebar_App.test.tsx
@@ -5,7 +5,7 @@ import { App } from "../../components";
 describe("sidebar", () => {
   test("renders the correct cartridge bundle", () => {
     render(<App />);
-    const linkElement = screen.getByText(/sidebar/i);
+    const linkElement = screen.getByText(/Select an image/i);
 
     expect(linkElement).toBeInTheDocument();
   });
