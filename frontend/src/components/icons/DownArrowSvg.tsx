a8cb8bdbab550222a7373e4c9023c2e547f3f859
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-02 11:14:19 +0100

    chore: remove empty props

--- frontend/src/components/icons/DownArrowSvg.tsx
+++ frontend/src/components/icons/DownArrowSvg.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "../../styles/DownArrowSvg.css";
-interface Props {}
 
-export const DownArrowSvg = ({}: Props) => {
+export const DownArrowSvg = () => {
   return (
     <svg id="assets-arrow" viewBox="0 0 10 7">
       <path d="M5 6.4L.3 1.7 1.7.3 5 3.6 8.3.3l1.4 1.4z"></path>
