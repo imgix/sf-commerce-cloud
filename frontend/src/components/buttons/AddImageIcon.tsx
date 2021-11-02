d089965efd1e1e239350c1de5f1d762bf8b17556
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-02 11:14:19 +0100

    chore: update sidebar app to not break build and load image from imgix

--- frontend/src/components/buttons/AddImageIcon.tsx
+++ frontend/src/components/buttons/AddImageIcon.tsx
@@ -3,15 +3,12 @@ import { Button } from "./Button";
 
 interface Props {
   handleClick?: () => void;
-  imageSrc: string;
+  imageSrc?: string;
 }
 
 export function AddImageIcon({ handleClick, imageSrc }: Props): ReactElement {
   return (
     <div className="ix-select-image-container">
-      <div className="ix-select-image-btn" onClick={handleClick}>
-        <img width="200" height="150" src={imageSrc} alt="camera-svg-icon" />
-      </div>
       <Button label="Select an image from Origin" onClick={handleClick} />
     </div>
   );
