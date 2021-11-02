d7c74c841f6ac49c1e6ba2b64fd62a15022198b2
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-02 11:14:19 +0100

    chore: build in SF scripts into frontend build

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
 import { App } from "./components/";
-import reportWebVitals from "./reportWebVitals";
+import "./styles/index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+declare const listen: Function;
+
+var rootEditorElement;
+/**
+ * Initializes the base markup before page is ready. This is not part of the API, and called explicitly at the end of this module.
+ */
+function init() {
+  rootEditorElement = document.createElement("div");
+  rootEditorElement.innerHTML = `<h3>Should be replaced by React</h3>`;
+  document.body.appendChild(rootEditorElement);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEditorElement
+  );
+}
+
+listen("sfcc:ready", () => {
+  init();
+});
+
+// When a value was selected
+listen("sfcc:value", (value: any) => {});
+// When the editor must require the user to select something
+listen("sfcc:required", (value: any) => {});
+// When the editor is asked to disable its controls
+listen("sfcc:disabled", (value: any) => {});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
