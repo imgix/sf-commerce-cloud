d26fd62eccd16e97b96bbfaf132346a01333511d
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-02 11:14:19 +0100

    test: create test dirs for each app

--- frontend/src/tests/sidebarApp/sidebar_App.test.tsx
+++ frontend/src/tests/sidebarApp/sidebar_App.test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { App } from "../components";
+import { App } from "../../components";
 
-describe("breakout", () => {
+describe("sidebar", () => {
   test("renders the correct cartridge bundle", () => {
     render(<App />);
-    const linkElement = screen.getByText(/breakout/i);
+    const linkElement = screen.getByText(/sidebar/i);
 
     expect(linkElement).toBeInTheDocument();
   });
