cdd1373566d23ccc742d6c51958b5fbd388d6f4a
Luis Ball <lball@imgix.com>
2021-11-03 15:59:03 -0700

    build: change css bundle filename
    This commit uses the mini-css-extract-plugin to give the react app css
    bundles a predicatble and unique filename. This is done so that a future
    build script can reference this filename when copying over the styles
    into the sfcc cartridge.

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,9 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = function override(config, env) {
+  console.log(env);
   config.optimization.splitChunks = {
     cacheGroups: {
       default: false,
     },
   };
+  const appName = process.env.REACT_APP_APP_TYPE || "react";
   config.optimization.runtimeChunk = false;
+  config.output.filename = `js/${appName}.js`;
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: `css/${appName}.css`,
+    })
+  );
   return config;
 };
