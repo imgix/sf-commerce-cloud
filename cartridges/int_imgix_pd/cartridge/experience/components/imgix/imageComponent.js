0a339694ae732cec549557589fa1c8c46cd9ad8c
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-04 15:29:36 +0100

    chore: render test image from component

--- cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
+++ cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
@@ -1,21 +1,24 @@
 "use strict";
-// These all are SFCC APIs
 var Template = require("dw/util/Template");
 var HashMap = require("dw/util/HashMap");
-// we included an existing SFCC back-end script to handle the image path
-var ImageTransformation = require("*/cartridge/experience/utilities/ImageTransformation.js");
+var Logger = require("dw/system/Logger");
 
 // handle function for component.
 module.exports.render = function (context, modelIn) {
+  const ImgixLogger = Logger.getLogger("imgix");
+  ImgixLogger.info(
+    "************************** imgix Image Component Start Render"
+  );
+
   var model = modelIn || new HashMap();
   var content = context.content;
 
-  model.title = content.title ? content.title : null;
-  model.image = ImageTransformation.getScaledImage(content.image);
   // use to give link on the image, if we click on image it take to us to that page.
   model.link = content.imageLink ? content.imageLink : "#";
   model.alt = content.alt ? content.alt : null;
+  model.image_src = "https://assets.imgix.net/amsterdam.jpg?w=500";
 
-  return new Template("/imgix/image/imagerenderer/imagerenderer").render(model)
-    .text;
+  return new Template("/experience/components/imgix/imageComponent").render(
+    model
+  ).text;
 };
