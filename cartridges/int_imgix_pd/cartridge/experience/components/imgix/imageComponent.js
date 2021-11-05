dd6ac8754bceb9ab853fbd7e3886fe81f2871592
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-05 17:49:06 +0100

    chore: use @imgix/js-core to build image src

--- cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
+++ cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
@@ -2,6 +2,7 @@
 var Template = require("dw/util/Template");
 var HashMap = require("dw/util/HashMap");
 var Logger = require("dw/system/Logger");
+var ImgixClient = require("*/cartridge/scripts/jsCore/jsCore");
 
 // handle function for component.
 module.exports.render = function (context, modelIn) {
@@ -16,8 +17,9 @@ module.exports.render = function (context, modelIn) {
   // use to give link on the image, if we click on image it take to us to that page.
   model.link = content.imageLink ? content.imageLink : "#";
   model.alt = content.alt ? content.alt : null;
-  model.image_src =
+  const rawImageUrl =
     content.image_url || "https://assets.imgix.net/amsterdam.jpg?w=500";
+  model.image_src = ImgixClient._buildURL(rawImageUrl, { txt: "Hello World" });
 
   return new Template("/experience/components/imgix/imageComponent").render(
     model
