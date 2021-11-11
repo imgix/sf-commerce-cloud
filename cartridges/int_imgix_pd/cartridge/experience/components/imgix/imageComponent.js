86b86f0a3f0ef936ea7331c00c8577b1aa8081c5
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-11 12:50:03 +0100

    chore: set srcset, sizes, and ixlib

--- cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
+++ cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
@@ -14,12 +14,22 @@ module.exports.render = function (context, modelIn) {
   var model = modelIn || new HashMap();
   var content = context.content;
 
+  const ixlib = "sfccPD";
+
   // use to give link on the image, if we click on image it take to us to that page.
   model.link = content.imageLink ? content.imageLink : "#";
   model.alt = content.alt ? content.alt : null;
   const rawImageUrl =
     content.image_url || "https://assets.imgix.net/amsterdam.jpg?w=500";
-  model.image_src = ImgixClient._buildURL(rawImageUrl, { txt: "Hello World" });
+  model.image_src = ImgixClient._buildURL(rawImageUrl, {
+    auto: "format,compress",
+    ixlib: ixlib,
+  });
+  model.image_srcset = ImgixClient._buildSrcSet(rawImageUrl, {
+    auto: "format,compress",
+    ixlib: ixlib,
+  });
+  model.image_sizes = content.sizes;
 
   return new Template("/experience/components/imgix/imageComponent").render(
     model
