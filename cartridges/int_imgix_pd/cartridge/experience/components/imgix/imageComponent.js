bde6314cf8075d0716a26e4ff8d056fc7dc107b7
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-11 13:05:38 +0100

    chore: set ixlib correctly and include component version

--- cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
+++ cartridges/int_imgix_pd/cartridge/experience/components/imgix/imageComponent.js
@@ -3,6 +3,7 @@ var Template = require("dw/util/Template");
 var HashMap = require("dw/util/HashMap");
 var Logger = require("dw/system/Logger");
 var ImgixClient = require("*/cartridge/scripts/jsCore/jsCore");
+var version = require("*/cartridge/scripts/imgix/version.json");
 
 // handle function for component.
 module.exports.render = function (context, modelIn) {
@@ -14,21 +15,33 @@ module.exports.render = function (context, modelIn) {
   var model = modelIn || new HashMap();
   var content = context.content;
 
-  const ixlib = "sfccPD";
+  const ixlib = "sfccPD-" + version.version;
 
   // use to give link on the image, if we click on image it take to us to that page.
   model.link = content.imageLink ? content.imageLink : "#";
   model.alt = content.alt ? content.alt : null;
   const rawImageUrl =
     content.image_url || "https://assets.imgix.net/amsterdam.jpg?w=500";
-  model.image_src = ImgixClient._buildURL(rawImageUrl, {
-    auto: "format,compress",
-    ixlib: ixlib,
-  });
-  model.image_srcset = ImgixClient._buildSrcSet(rawImageUrl, {
-    auto: "format,compress",
-    ixlib: ixlib,
-  });
+  model.image_src = ImgixClient._buildURL(
+    rawImageUrl,
+    {
+      auto: "format,compress",
+    },
+    {
+      includeLibraryParam: false,
+      libraryParam: ixlib,
+    }
+  );
+  model.image_srcset = ImgixClient._buildSrcSet(
+    rawImageUrl,
+    {
+      auto: "format,compress",
+    },
+    {
+      includeLibraryParam: false,
+      libraryParam: ixlib,
+    }
+  );
   model.image_sizes = content.sizes;
 
   return new Template("/experience/components/imgix/imageComponent").render(
