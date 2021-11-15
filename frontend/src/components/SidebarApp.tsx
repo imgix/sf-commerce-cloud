00ca3669a98c56d70b30b9b6984cfc4f23f60e90
hasanbadshah <syed.badshah@toptal.com>
2021-11-15 16:47:27 +0000

    Accessing Imag link in sidebar

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -1,17 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 import Imgix from "react-imgix";
 // TODO(luis): replace placeholder image
 import "../styles/App.css";
 import { AddImageIcon } from "./buttons/AddImageIcon";
 
-type HandleBreakoutOpenT = () => (event: string, type: string) => void;
+declare const emit: Function;
+declare const subscribe: Function;
 
-export function App({
-  handleBreakoutOpen,
-}: {
-  handleBreakoutOpen: HandleBreakoutOpenT;
-}) {
-  return (
+export function App() {
+    const [imgUrl, setImgUrl] = useState('-')
+    let localization: any;
+    let buttonEl: any;
+
+    subscribe(
+        "sfcc:ready",
+        async ({
+                   value,
+                   config,
+                   isDisabled,
+                   isRequired,
+                   dataLocale,
+                   displayLocale,
+               }: any) => {
+            // Extract `localization` data from `config`
+            ({ localization = {} } = config);
+        }
+    );
+    function handleBreakoutApply(value: any) {
+        emit({
+            type: "sfcc:value",
+            payload: value,
+        });
+        setImgUrl(value.imgUrl)
+    }
+
+    function handleBreakoutCancel(value: any) {
+        // Grab focus
+        console.log(value, ' from cancel')
+        buttonEl && buttonEl.focus();
+    }
+
+    function handleBreakoutClose({ type, value }: any) {
+        // Now the "value" can be passed back to Page Designer
+        if (type === "sfcc:breakoutApply") {
+            handleBreakoutApply(value);
+        } else {
+            handleBreakoutCancel(value);
+        }
+    }
+
+    function handleBreakoutOpen() {
+        emit(
+            {
+                type: "sfcc:breakout",
+                payload: {
+                    id: "imgixEditorBreakoutScript",
+                    title: `${localization.titleBreakout}`,
+                },
+            },
+            handleBreakoutClose
+        );
+    }
+
+    return (
     <div className="App">
       <header className="App-header">
         <div>
@@ -21,6 +72,7 @@ export function App({
               w: 350,
             }}
           />
+            <input id={'selectedImgUrl'} value={imgUrl}/>
           <AddImageIcon handleClick={handleBreakoutOpen} />
         </div>
       </header>
--- frontend/src/index-sidebar.tsx
+++ frontend/src/index-sidebar.tsx
@@ -3,75 +3,21 @@ import ReactDOM from "react-dom";
 import { App } from "./components";
 import "./styles/index.css";
 
-declare const emit: Function;
-declare const subscribe: Function;
+
 
 var rootEditorElement;
 
 export const createSidebarApp = () => {
-  let localization: any;
-  let buttonEl: any;
 
-  subscribe(
-    "sfcc:ready",
-    async ({
-      value,
-      config,
-      isDisabled,
-      isRequired,
-      dataLocale,
-      displayLocale,
-    }: any) => {
-      // Extract `localization` data from `config`
-      ({ localization = {} } = config);
-
-      function handleBreakoutApply(value: any) {
-          console.log(value, ' from apply')
-        // Emit value update to Page Designer host application
-        emit({
-          type: "sfcc:value",
-          payload: value,
-        });
-      }
-
-      function handleBreakoutCancel(value: any) {
-        // Grab focus
-          console.log(value, ' from cancel')
-        buttonEl && buttonEl.focus();
-      }
-
-      function handleBreakoutClose({ type, value }: any) {
-        // Now the "value" can be passed back to Page Designer
-        if (type === "sfcc:breakoutApply") {
-          handleBreakoutApply(value);
-        } else {
-          handleBreakoutCancel(value);
-        }
-      }
-
-      function handleBreakoutOpen() {
-        emit(
-          {
-            type: "sfcc:breakout",
-            payload: {
-              id: "imgixEditorBreakoutScript",
-              title: `${localization.titleBreakout}`,
-            },
-          },
-          handleBreakoutClose
-        );
-      }
-
-      // Initialize the DOM
-      rootEditorElement = document.createElement("div");
-      rootEditorElement.innerHTML = `<h3>Sidebar: should be replaced by React</h3>`;
-      document.body.appendChild(rootEditorElement);
-      ReactDOM.render(
+    // Initialize the DOM
+    rootEditorElement = document.createElement("div");
+    rootEditorElement.innerHTML = `<h3>Sidebar: should be replaced by React</h3>`;
+    document.body.appendChild(rootEditorElement);
+    ReactDOM.render(
         <React.StrictMode>
-          <App handleBreakoutOpen={handleBreakoutOpen} />
+            <App />
         </React.StrictMode>,
         rootEditorElement
-      );
-    }
-  );
+    );
+
 };
