d3845420152f712118ad5319dd620d6adb8a450c
Luis Ball <lball@imgix.com>
2021-11-15 16:03:39 -0800

    chore: apply prettier linting

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Imgix from "react-imgix";
 // TODO(luis): replace placeholder image
 import "../styles/App.css";
@@ -8,61 +8,61 @@ declare const emit: Function;
 declare const subscribe: Function;
 
 export function App() {
-    const [imgUrl, setImgUrl] = useState('-')
-    let localization: any;
-    let buttonEl: any;
+  const [imgUrl, setImgUrl] = useState("-");
+  let localization: any;
+  let buttonEl: any;
 
-    subscribe(
-        "sfcc:ready",
-        async ({
-                   value,
-                   config,
-                   isDisabled,
-                   isRequired,
-                   dataLocale,
-                   displayLocale,
-               }: any) => {
-            // Extract `localization` data from `config`
-            ({ localization = {} } = config);
-        }
+  subscribe(
+    "sfcc:ready",
+    async ({
+      value,
+      config,
+      isDisabled,
+      isRequired,
+      dataLocale,
+      displayLocale,
+    }: any) => {
+      // Extract `localization` data from `config`
+      ({ localization = {} } = config);
+    }
+  );
+  function handleBreakoutApply(value: any) {
+    emit({
+      type: "sfcc:value",
+      payload: value,
+    });
+  }
+
+  function handleBreakoutCancel(value: any) {
+    // Grab focus
+    console.log(value, " from cancel");
+    buttonEl && buttonEl.focus();
+  }
+
+  function handleBreakoutClose({ type, value }: any) {
+    setImgUrl(value.imgUrl);
+    // Now the "value" can be passed back to Page Designer
+    if (type === "sfcc:breakoutApply") {
+      handleBreakoutApply(value);
+    } else {
+      handleBreakoutCancel(value);
+    }
+  }
+
+  function handleBreakoutOpen() {
+    emit(
+      {
+        type: "sfcc:breakout",
+        payload: {
+          id: "imgixEditorBreakoutScript",
+          title: `${localization.titleBreakout}`,
+        },
+      },
+      handleBreakoutClose
     );
-    function handleBreakoutApply(value: any) {
-        emit({
-            type: "sfcc:value",
-            payload: value,
-        });
-    }
+  }
 
-    function handleBreakoutCancel(value: any) {
-        // Grab focus
-        console.log(value, ' from cancel')
-        buttonEl && buttonEl.focus();
-    }
-
-    function handleBreakoutClose({ type, value }: any) {
-        setImgUrl(value.imgUrl)
-        // Now the "value" can be passed back to Page Designer
-        if (type === "sfcc:breakoutApply") {
-            handleBreakoutApply(value);
-        } else {
-            handleBreakoutCancel(value);
-        }
-    }
-
-    function handleBreakoutOpen() {
-        emit(
-            {
-                type: "sfcc:breakout",
-                payload: {
-                    id: "imgixEditorBreakoutScript",
-                    title: `${localization.titleBreakout}`,
-                },
-            },
-            handleBreakoutClose
-        );
-    }
-
-    return (
+  return (
     <div className="App">
       <header className="App-header">
         <div>
@@ -72,7 +72,11 @@ export function App() {
               w: 350,
             }}
           />
-            <input id={'selectedImgUrl'} style={{ border: 'solid black 1px'}} value={imgUrl}/>
+          <input
+            id={"selectedImgUrl"}
+            style={{ border: "solid black 1px" }}
+            value={imgUrl}
+          />
           <AddImageIcon handleClick={handleBreakoutOpen} />
         </div>
       </header>
