2997c0a97c858b2708a6be247e6542ad0e06ef9e
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-16 16:35:08 +0100

    chore: remove todo

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Imgix from "react-imgix";
-// TODO(luis): replace placeholder image
 import "../styles/App.css";
 import { IImgixCustomAttributeValue } from "../types/imgixSF";
 import { AddImageIcon } from "./buttons/AddImageIcon";
