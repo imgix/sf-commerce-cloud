91c8eda90e0478feeb28676f918d7576d98c99e3
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-16 16:12:40 +0100

    feat: store current custom attribute value and display img

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import Imgix from "react-imgix";
 // TODO(luis): replace placeholder image
 import "../styles/App.css";
+import { IBreakoutPayload } from "../types/imgixSF";
 import { AddImageIcon } from "./buttons/AddImageIcon";
 
 export function App({
   handleBreakoutOpen,
+  value,
 }: {
   handleBreakoutOpen: () => void;
+  value: IBreakoutPayload;
 }) {
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <Imgix
-            src="https://assets.imgix.net/amsterdam.jpg"
+            src={value.src}
             imgixParams={{
               w: 350,
             }}
