3f019b2daab874e9974a65844bdb6f8eab1a403d
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-17 14:35:52 +0100

    chore: fix type mismatch in sidebarapp story

--- frontend/src/stories/SidebarApp.tsx
+++ frontend/src/stories/SidebarApp.tsx
@@ -1,10 +1,8 @@
 import React, { ReactElement } from "react";
-import { App as _SidebarApp } from "../components/SidebarApp";
+import { App as _SidebarApp, ISidebarAppProps } from "../components/SidebarApp";
 import styles from "./SidebarApp.module.css";
 
-interface Props {}
-
-export function SidebarApp({ ...otherProps }: Props): ReactElement {
+export function SidebarApp({ ...otherProps }: ISidebarAppProps): ReactElement {
   return (
     <div className={styles.storyWrapper}>
       <_SidebarApp {...otherProps} />
