bb6ce3ed80a4aa0dc2928d99b906c03460a041ae
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-17 20:18:47 +0100

    chore: set sizes value correctly for images in AssetGrid
    Co-authored-by: Luis Ball <lball@imgix.com>

--- frontend/src/components/grids/AssetGrid.tsx
+++ frontend/src/components/grids/AssetGrid.tsx
@@ -47,8 +47,21 @@ export function AssetGrid({
               fit: "crop",
               crop: "entropy",
             }}
-            // TODO: ensure sizes is set correctly
-            sizes="(min-width: 480px) calc(12.5vw - 20px)"
+            /* This sizes attribute is a monster and sets the size of the image
+             * correctly, handling both the SF breakpoints and the design
+             * breakpoints
+             * The SF modal has a breakpoint at 768px (48rem), below which the
+             * modal has a margin around it of 32px, and above which it has a
+             * margin of 5% each side.
+             * In these calculates, the format is:
+             * calc((100vw - SFmargin - modalMargin - betweenColumnMarginSum)/numColumns)
+             * */
+            sizes="(max-width: 500px) calc((100vw - 64px - 32px - 16px)/2),
+            (max-width: 700px) calc((100vw - 64px - 32px - 32px)/3),
+            (max-width: 768px) calc((100vw - 64px - 32px - 48px)/4),
+            (max-width: 820px) calc((100vw - 10vw - 32px - 48px)/4),
+            (max-width: 960px) calc((100vw - 10vw - 32px - 80px)/6),
+            calc((100vw - 10vw - 32px - 96px)/7)"
           />
         </div>
         <p className="ix-grid-item-filename">
