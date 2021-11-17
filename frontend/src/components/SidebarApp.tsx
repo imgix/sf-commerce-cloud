89ee6e7db037cc7116609373002cb92481994b66
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-17 14:35:52 +0100

    chore: fix type mismatch in sidebarapp story

--- frontend/src/components/SidebarApp.tsx
+++ frontend/src/components/SidebarApp.tsx
@@ -4,13 +4,12 @@ import "../styles/App.css";
 import { IImgixCustomAttributeValue } from "../types/imgixSF";
 import { AddImageIcon } from "./buttons/AddImageIcon";
 
-export function App({
-  handleBreakoutOpen,
-  value,
-}: {
+export type ISidebarAppProps = {
   handleBreakoutOpen: () => void;
   value: IImgixCustomAttributeValue | undefined;
-}) {
+};
+
+export function App({ handleBreakoutOpen, value }: ISidebarAppProps) {
   return (
     <div className="App">
       <header className="App-header">
