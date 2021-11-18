86b8e12304da6c34d6dfd6c3c3b1fb55a7db6dac
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-18 20:15:59 +0100

    chore: add sidebar caption and ability to remove image

--- frontend/src/tests/sidebarApp/sidebar_App.test.tsx
+++ frontend/src/tests/sidebarApp/sidebar_App.test.tsx
@@ -4,7 +4,13 @@ import { App as SidebarApp } from "../../components/SidebarApp";
 
 describe("sidebar", () => {
   test("renders the correct cartridge bundle", () => {
-    render(<SidebarApp value={undefined} handleBreakoutOpen={() => {}} />);
+    render(
+      <SidebarApp
+        value={undefined}
+        onOpenBreakoutClick={() => {}}
+        onClear={() => {}}
+      />
+    );
     const linkElement = screen.getByText(/Add image/i);
 
     expect(linkElement).toBeInTheDocument();
