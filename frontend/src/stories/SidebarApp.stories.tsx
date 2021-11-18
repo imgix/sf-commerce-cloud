5c894e6703e41666ddca70fa139a674275a48f68
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-18 20:15:30 +0100

    chore: reduce number of files in sidebar app story

--- frontend/src/stories/SidebarApp.stories.tsx
+++ frontend/src/stories/SidebarApp.stories.tsx
@@ -1,21 +1,40 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import { SidebarApp } from "./SidebarApp";
+import { App as SidebarApp } from "../components/SidebarApp";
 
 export default {
   title: "Example/SidebarApp",
   component: SidebarApp,
-  argTypes: { onSelectAsset: { action: "selected" } },
+  decorators: [
+    (Story: any) => (
+      <div
+        style={{
+          width: 368,
+          boxShadow: "-1px 0 3px 0 rgb(0 0 0 / 25%)",
+          padding: 12,
+          margin: "12px auto",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof SidebarApp>;
 
 const Template: ComponentStory<typeof SidebarApp> = (args) => (
   <SidebarApp {...args} />
 );
 
-export const Default = Template.bind({});
-Default.args = {};
-
 export const EmptyState = Template.bind({});
-Default.args = {
+EmptyState.args = {
   value: undefined,
+};
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+  value: {
+    src: "https://assets.imgix.net/amsterdam.jpg",
+    mediaWidth: 2000,
+    mediaHeight: 1000,
+  },
 };
