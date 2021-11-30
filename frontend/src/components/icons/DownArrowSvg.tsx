af44796a27193ae0c30d7c1bb9860352553bd320
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-11-30 16:03:20 +0100

    chore: use modular CSS for down arrow svg

--- frontend/src/components/icons/DownArrowSvg.tsx
+++ frontend/src/components/icons/DownArrowSvg.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "../../styles/DownArrowSvg.css";
+import styles from "./DownArrowSvg.module.scss";
 
 export const DownArrowSvg = () => {
   return (
-    <svg id="assets-arrow" viewBox="0 0 10 7">
+    <svg className={styles.svg} viewBox="0 0 10 7">
       <path d="M5 6.4L.3 1.7 1.7.3 5 3.6 8.3.3l1.4 1.4z"></path>
     </svg>
   );
