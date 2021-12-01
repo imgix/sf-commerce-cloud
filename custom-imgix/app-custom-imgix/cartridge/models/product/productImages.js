5def7e5dc89af1130f34d25fda0b40c40d666a01
hasanbadshah <syed.badshah@toptal.com>
2021-12-01 13:32:15 +0000

    Created Imgix Custom Cartridge

---
+++ custom-imgix/app-custom-imgix/cartridge/models/product/productImages.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var collections = require('*/cartridge/scripts/util/collections');
+const currentSite = require('dw/system/Site').getCurrent();
+/**
+ * @constructor
+ * @classdesc Returns images for a given product
+ * @param {dw.catalog.Product} product - product to return images for
+ * @param {Object} imageConfig - configuration object with image types
+ */
+function Images(product, imageConfig) {
+    const imgixBaseURL = currentSite.getCustomPreferenceValue('imgixBaseURL')
+    imageConfig.types.forEach(function (type) {
+        var images = product.getImages(type);
+        var result = {};
+
+        if (imageConfig.quantity === 'single') {
+            var firstImage = collections.first(images);
+            if (firstImage) {
+                result = [{
+                    alt: firstImage.alt,
+                    url: firstImage.URL.toString(),  // TODO: Append imgixBaseURL
+                    title: firstImage.title,
+                    index: '0',
+                    absURL: firstImage.absURL.toString()
+                }];
+            }
+        } else {
+            result = collections.map(images, function (image, index) {
+                return {
+                    alt: image.alt,
+                    url: image.URL.toString(), // TODO: Append imgixBaseURL
+                    index: index.toString(),
+                    title: image.title,
+                    absURL: image.absURL.toString()
+                };
+            });
+        }
+        this[type] = result;
+    }, this);
+}
+
+module.exports = Images;
