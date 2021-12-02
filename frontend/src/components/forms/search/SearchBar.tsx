7ef4d341c3011ee8c4e8e7f97aa8af7274f40fdc
Luis Ball <lball@imgix.com>
2021-12-02 09:23:40 -0800

    feat(search): use hooks to style on input focus

--- frontend/src/components/forms/search/SearchBar.tsx
+++ frontend/src/components/forms/search/SearchBar.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from "react";
 import { SearchIconSvg } from "../../icons/SearchIconSvg";
-import "../../../styles/Form.css";
+import { useClickOutside } from "./useClickOutside";
+import { useFocus } from "./useFocus";
+import styles from "./SearchBar.module.scss";
 
 interface Props {
   placeholder?: string;
@@ -9,14 +11,20 @@ interface Props {
 
 export function SearchBar({ placeholder, handleSubmit }: Props): ReactElement {
   const [query, setQuery] = React.useState("");
+  const { visibleRef, isVisible, setIsVisible } = useClickOutside(false);
+  const { focusRef, isFocused, setIsFocused } = useFocus(false);
+  // TODO: remove this, used for testing
+  // make a list of placeholder searches
+  const placeholderSearches = ["Apple", "Banana", "Cherry"];
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setQuery(e.currentTarget.value);
   };
+
   return (
     <form
-      className="ix-asset-simple-search-content"
+      className={styles.searchContent}
       onSubmit={(e) => {
         // Prevent the form from submitting, i.e. reloading the page
         e.preventDefault();
@@ -24,15 +32,16 @@ export function SearchBar({ placeholder, handleSubmit }: Props): ReactElement {
         handleSubmit(query);
       }}
     >
-      <div className="ix-asset-simple-search-wrapper">
-        <div className="ix-asset-simple-search-base">
-          <div className="ix-asset-simple-search-base-input">
-            <div className="ix-asset-simple-search-icon">
+      <div ref={visibleRef} className={styles.searchWrapper}>
+        <div className={styles.searchBase}>
+          <div className={styles.searchBaseInput}>
+            <div className={styles.simpleSearchIcon}>
               <SearchIconSvg />
             </div>
             <input
+              ref={focusRef}
               type="text"
-              className="ix-asset-simple-search-base-input-field"
+              className={styles.searchBaseInputField}
               placeholder={
                 placeholder ? placeholder : "Search filename or path"
               }
@@ -47,7 +56,34 @@ export function SearchBar({ placeholder, handleSubmit }: Props): ReactElement {
                 event.preventDefault();
                 handleSubmit(query);
               }}
+              onClick={() => {
+                setIsVisible(true);
+              }}
             />
+          </div>
+        </div>
+        <div className={isFocused ? styles.show : styles.hide}>
+          <div className={styles.searchBaseSuggestionsList}>
+            <p>--------------</p>
+            <p>Recent Searches</p>
+            {placeholderSearches.map((search) => (
+              <div
+                className={styles.searchBaseSuggestionsListItem}
+                key={search}
+                onClick={() => {
+                  setQuery(search);
+                  handleSubmit(search);
+                }}
+              >
+                {" "}
+                <div className={styles.searchBaseSuggestionsListItem}>
+                  <div className={styles.simpleSearchIcon}>
+                    <SearchIconSvg />
+                  </div>
+                  {search}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
