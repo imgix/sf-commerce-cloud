0f2373ff1a2f76aa7f01ec4e6185c99cc98341e9
hasanbadshah <syed.badshah@toptal.com>
2021-12-07 13:06:49 +0000

    Renamed cartridge name to follow standard

--- custom_imgix/app_custom_imgix/cartridge/models/product/productImages.js
+++ custom_imgix/app_custom_imgix/cartridge/models/product/productImages.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var collections = require('*/cartridge/scripts/util/collections');
+var Logger = require("dw/system/Logger");
 const currentSite = require('dw/system/Site').getCurrent();
 /**
  * @constructor
@@ -8,18 +9,28 @@ const currentSite = require('dw/system/Site').getCurrent();
  * @param {dw.catalog.Product} product - product to return images for
  * @param {Object} imageConfig - configuration object with image types
  */
+
+
 function Images(product, imageConfig) {
     const imgixBaseURL = currentSite.getCustomPreferenceValue('imgixBaseURL')
+    const ImgixLogger = Logger.getLogger("imgixCustom");
+    const imgixData = product.custom.attribute.imgixData
+
+    if (imgixData !== null){
+        const imgixJsonImages = JSON.parse(imgixData)
+        ImgixLogger.info(imgixJsonImages.images[0].image_url)
+    }
+    var result = {};
+
     imageConfig.types.forEach(function (type) {
         var images = product.getImages(type);
-        var result = {};
-
         if (imageConfig.quantity === 'single') {
             var firstImage = collections.first(images);
             if (firstImage) {
                 result = [{
                     alt: firstImage.alt,
                     url: imgixBaseURL + firstImage.URL.toString(),
+                    // url: imgixJsonImages === null ? (imgixBaseURL + firstImage.URL.toString()) : imgixJsonImages.images[0].img_url,
                     title: firstImage.title,
                     index: '0',
                     absURL: firstImage.absURL.toString()
