6e90bc7944584dec1071123d5477ec0f0a57c363
Chhaykun Lai (Osf-commerce) <lai.chhaykun@osf.digital>
2021-12-08 20:53:24 +0700

    Get imgixData custom attribute to display image for product

--- custom_imgix/app_custom_imgix/cartridge/models/product/productImages.js
+++ custom_imgix/app_custom_imgix/cartridge/models/product/productImages.js
@@ -2,6 +2,7 @@
 
 var collections = require('*/cartridge/scripts/util/collections');
 const currentSite = require('dw/system/Site').getCurrent();
+
 /**
  * @constructor
  * @classdesc Returns images for a given product
@@ -9,7 +10,16 @@ const currentSite = require('dw/system/Site').getCurrent();
  * @param {Object} imageConfig - configuration object with image types
  */
 function Images(product, imageConfig) {
-    const imgixBaseURL = currentSite.getCustomPreferenceValue('imgixBaseURL')
+    const imgixBaseURL = currentSite.getCustomPreferenceValue('imgixBaseURL');
+    let imgixJsonImages = null;
+
+    if (product instanceof require('dw/catalog/ProductVariationModel')) {
+        const productVariant = product.selectedVariant || product.defaultVariant;
+        imgixJsonImages = productVariant && productVariant.custom && productVariant.custom.imgixData && JSON.parse(product.selectedVariant.custom.imgixData);
+    } else if (product instanceof require('dw/catalog/Variant') || product instanceof require('dw/catalog/Product')) {
+        imgixJsonImages = product.custom && product.custom.imgixData && JSON.parse(product.custom.imgixData);
+    }
+
     imageConfig.types.forEach(function (type) {
         var images = product.getImages(type);
         var result = {};
@@ -17,9 +27,13 @@ function Images(product, imageConfig) {
         if (imageConfig.quantity === 'single') {
             var firstImage = collections.first(images);
             if (firstImage) {
+                let imageUrl = imgixBaseURL + firstImage.URL.toString();
+                if (imgixJsonImages) {
+                    imageUrl = imgixJsonImages.images[0].image_url;
+                }
                 result = [{
                     alt: firstImage.alt,
-                    url: imgixBaseURL + firstImage.URL.toString(),
+                    url: imageUrl,
                     title: firstImage.title,
                     index: '0',
                     absURL: firstImage.absURL.toString()
@@ -27,9 +41,17 @@ function Images(product, imageConfig) {
             }
         } else {
             result = collections.map(images, function (image, index) {
+                let imageUrl = imgixBaseURL + image.URL.toString();
+                if (imgixJsonImages) {
+                    if (index === 0) {
+                        imageUrl = imgixJsonImages.images[0].image_url;
+                    } else {
+                        imageUrl = imgixJsonImages.images[1].image_url;
+                    }
+                }
                 return {
                     alt: image.alt,
-                    url: imgixBaseURL + image.URL.toString(),
+                    url: imageUrl,
                     index: index.toString(),
                     title: image.title,
                     absURL: image.absURL.toString()
