4f22803ec8c01f5e5b6c488a454e1042caa6af12
Luis Ball <lball@imgix.com>
2021-12-09 08:29:46 -0800

    chore: remove duplicated style
    This commit removes the className prop from the SVG element. This was
    committed unintentionally and was causing this icon to be too small.

--- frontend/src/components/icons/WebProxyIcon.tsx
+++ frontend/src/components/icons/WebProxyIcon.tsx
@@ -8,10 +8,7 @@ export const WebProxyIcon = ({
 }): ReactElement => {
   return (
     <div className={styles.container + (className ? ` ${className}` : "")}>
-      <svg
-        className={styles.svg + (className ? ` ${className}` : "")}
-        viewBox="0 0 32 32"
-      >
+      <svg width="16" height="16" viewBox="0 0 16 16">
         <path
           d="M16 2V14H14C13.448 14 13 13.552 13 13V12H3V13C3 13.552 2.552 14 2 14H0V2H2C2.552 2 3 2.448 3 3V4H13V3C13 2.448 13.448 2 14 2H16ZM5 7H3V9H5V7ZM9 7H7V9H9V7ZM13 7H11V9H13V7Z"
           fill="black"
