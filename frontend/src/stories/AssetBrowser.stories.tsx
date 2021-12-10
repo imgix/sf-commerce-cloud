9703dc95dd2d69e93a221903e8d24461cefb1ccd
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-12-10 13:54:26 +0100

    chore: better integrate the new pagination component with the AssetBrowser
    - optimistically update cursor
    - update data structure to better work with new pagination component
    - disable the pagination component when loading data
    - hide the pagination component when no assets exist

--- frontend/src/stories/AssetBrowser.stories.tsx
+++ frontend/src/stories/AssetBrowser.stories.tsx
@@ -7,28 +7,13 @@ export default {
   title: "Example/AssetBrowser",
   component: AssetBrowser,
   argTypes: { onSelectAsset: { action: "selected" } },
-  parameters: {
-    layout: "centered",
-  },
 } as ComponentMeta<typeof AssetBrowser>;
 
-const Template: ComponentStory<typeof AssetBrowser> = (args) => (
-  <AssetBrowser {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  apiKey: "",
-};
-
-const SmallTemplate: ComponentStory<typeof AssetBrowserContainer> = (args) => (
+const Template: ComponentStory<typeof AssetBrowserContainer> = (args) => (
   <div
     style={{
       position: "absolute",
-      top: 32,
-      right: 32,
-      bottom: 32,
-      left: 32,
+      inset: 0,
       background: "#fff",
       display: "flex",
       flexDirection: "column",
@@ -38,7 +23,7 @@ const SmallTemplate: ComponentStory<typeof AssetBrowserContainer> = (args) => (
   </div>
 );
 
-export const SmallContainer = SmallTemplate.bind({});
-SmallContainer.args = {
+export const Default = Template.bind({});
+Default.args = {
   apiKey: "",
 };
