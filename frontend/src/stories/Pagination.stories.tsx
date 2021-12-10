2f00eabbe0f5d72ab68c63be7466d3088262cf4e
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-12-10 12:54:40 +0100

    chore: use MultiStory helper

--- frontend/src/stories/Pagination.stories.tsx
+++ frontend/src/stories/Pagination.stories.tsx
@@ -1,20 +1,39 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
 import { Pagination as PaginationComponent } from "../components/Pagination/Pagination";
+import { MultiStory } from "./common/MultiStory";
 
 export default {
   title: "Example/Pagination",
   component: PaginationComponent,
-  parameters: {
-    layout: "centered",
-  },
+  argTypes: { onPageChange: { action: "page changed" } },
 } as ComponentMeta<typeof PaginationComponent>;
 
 const Template: ComponentStory<typeof PaginationComponent> = (args) => (
-  <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
-    <PaginationComponent {...args} />
-  </div>
+  <MultiStory
+    stories={[
+      {
+        label: "Normal",
+        story: <PaginationComponent {...args} />,
+        options: { style: { width: 223 } },
+      },
+      {
+        label: "300px wide",
+        story: <PaginationComponent {...args} />,
+        options: { style: { width: 300 } },
+      },
+    ]}
+    style={{ background: "#e3e7eb" }}
+  ></MultiStory>
 );
 
 export const Pagination = Template.bind({});
-Pagination.args = {};
+Pagination.args = {
+  cursor: {
+    current: "1",
+    next: "2",
+    limit: 100,
+    hasMore: true,
+    totalRecords: 10,
+  },
+};
