af33be1437ac79769f97a738da412bb4bce53352
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-12-10 12:54:31 +0100

    chore: add MultiStory helper

---
+++ frontend/src/stories/common/MultiStory.tsx
@@ -0,0 +1,53 @@
+export type IStory = {
+  label: string;
+  story: React.ReactNode;
+  options?: {
+    flex?: boolean;
+    style?: React.CSSProperties;
+  };
+};
+
+export const MultiStory = ({
+  stories,
+  style,
+  spacingMultiplier = 1,
+}: {
+  stories: IStory[];
+  style?: React.CSSProperties;
+  spacingMultiplier?: number;
+}) => (
+  <div
+    style={{
+      minHeight: "100vw",
+      padding: 16 * spacingMultiplier,
+      ...style,
+    }}
+  >
+    {stories.map((story) => (
+      <div>
+        <div
+          style={{
+            fontSize: 12,
+            lineHeight: 1.66,
+            fontWeight: "bold",
+            color: "#6C7F8E",
+            textTransform: "uppercase",
+            marginBottom: 8 * spacingMultiplier,
+          }}
+        >
+          {story.label}
+        </div>
+        <div
+          style={{
+            ...(story.options?.flex === false ? {} : { display: "flex" }),
+            justifyContent: "flex-start",
+            ...story.options?.style,
+            marginBottom: 32 * spacingMultiplier,
+          }}
+        >
+          {story.story}
+        </div>
+      </div>
+    ))}
+  </div>
+);
