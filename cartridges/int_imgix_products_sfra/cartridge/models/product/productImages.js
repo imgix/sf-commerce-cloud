7bcb0699fc78b7cfff3db72da730a7541e26fba7
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-12-17 10:27:23 +0100

    feat: add default parameters to product images

--- cartridges/int_imgix_products_sfra/cartridge/models/product/productImages.js
+++ cartridges/int_imgix_products_sfra/cartridge/models/product/productImages.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-var collections = require('*/cartridge/scripts/util/collections');
-const currentSite = require('dw/system/Site').getCurrent();
+var collections = require("*/cartridge/scripts/util/collections");
+const currentSite = require("dw/system/Site").getCurrent();
 
 /**
  * @constructor
@@ -10,36 +10,87 @@ const currentSite = require('dw/system/Site').getCurrent();
  * @param {Object} imageConfig - configuration object with image types
  */
 function Images(product, imageConfig) {
-    const imgixBaseURL = currentSite.getCustomPreferenceValue('imgixBaseURL') || '';
+  const imgixBaseURL =
+    currentSite.getCustomPreferenceValue("imgixBaseURL") || "";
+  const isBaseURLSet = imgixBaseURL.trim().length > 0;
+  const imgixDefaultParams =
+    currentSite.getCustomPreferenceValue("imgixProductDefaultParams") || "";
 
+  if (isBaseURLSet) {
+    // Here we transform the URL
     imageConfig.types.forEach(function (type) {
-        var images = product.getImages(type);
-        var result = {};
+      var images = product.getImages(type);
+      var result = {};
 
-        if (imageConfig.quantity === 'single') {
-            var firstImage = collections.first(images);
-            if (firstImage) {
-                result = [{
-                    alt: firstImage.alt,
-                    url: imgixBaseURL + firstImage.URL.toString(),
-                    title: firstImage.title,
-                    index: '0',
-                    absURL: firstImage.absURL.toString()
-                }];
-            }
-        } else {
-            result = collections.map(images, function (image, index) {
-                return {
-                    alt: image.alt,
-                    url: imgixBaseURL + image.URL.toString(),
-                    index: index.toString(),
-                    title: image.title,
-                    absURL: image.absURL.toString()
-                };
-            });
+      if (imageConfig.quantity === "single") {
+        var firstImage = collections.first(images);
+        if (firstImage) {
+          result = [
+            {
+              alt: firstImage.alt,
+              url:
+                imgixBaseURL +
+                firstImage.URL.toString() +
+                (isBaseURLSet && imgixDefaultParams
+                  ? `?${imgixDefaultParams}`
+                  : ""),
+              title: firstImage.title,
+              index: "0",
+              absURL: firstImage.absURL.toString(),
+            },
+          ];
         }
-        this[type] = result;
+      } else {
+        result = collections.map(images, function (image, index) {
+          return {
+            alt: image.alt,
+            url:
+              imgixBaseURL +
+              image.URL.toString() +
+              (isBaseURLSet && imgixDefaultParams
+                ? `?${imgixDefaultParams}`
+                : ""),
+            index: index.toString(),
+            title: image.title,
+            absURL: image.absURL.toString(),
+          };
+        });
+      }
+      this[type] = result;
     }, this);
+  } else {
+    // Here we just pass through values
+    imageConfig.types.forEach(function (type) {
+      var images = product.getImages(type);
+      var result = {};
+
+      if (imageConfig.quantity === "single") {
+        var firstImage = collections.first(images);
+        if (firstImage) {
+          result = [
+            {
+              alt: firstImage.alt,
+              url: firstImage.URL.toString(),
+              title: firstImage.title,
+              index: "0",
+              absURL: firstImage.absURL.toString(),
+            },
+          ];
+        }
+      } else {
+        result = collections.map(images, function (image, index) {
+          return {
+            alt: image.alt,
+            url: firstImage.URL.toString(),
+            index: index.toString(),
+            title: image.title,
+            absURL: image.absURL.toString(),
+          };
+        });
+      }
+      this[type] = result;
+    }, this);
+  }
 }
 
 module.exports = Images;
