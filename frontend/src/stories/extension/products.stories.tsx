597016e8f14824282579e83d7e839762c3c396dc
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-01-13 16:12:00 +0100

    chore: copy SF HTML and CSS to Storybook for extension development

---
+++ frontend/src/stories/extension/products.stories.tsx
@@ -0,0 +1,91 @@
+import { ComponentMeta } from "@storybook/react";
+import React from "react";
+import { injectExtensionApp } from "../../index-extension";
+import "../../styles/App.css";
+import styles from "./products.module.scss";
+
+export const SFImgixProductsSection = () => {
+  return (
+    <table cellPadding="0" cellSpacing="0" style={{ width: "100%" }}>
+      <tbody>
+        <tr>
+          <td
+            className={`${styles.tableHeader} ${styles.aldi}`}
+            colSpan={3}
+            width="100%"
+          >
+            imgix
+          </td>
+        </tr>
+        <tr>
+          {/* Here was an image with 0 height... so I just removed it */}
+          <td colSpan={3}></td>
+        </tr>
+        <tr>
+          <td className={styles.top}>
+            <table cellSpacing="0" cellPadding="0" width="100%">
+              <tbody>
+                <tr>
+                  <td className={styles.fielditem2} align="right">
+                    <span
+                      data-dw-attruuid="48576099ecdd41387849fbe603"
+                      data-dw-tooltip="Product.imgixData"
+                    >
+                      imgix Images
+                    </span>
+                    :
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+          <td width="100%">
+            <table cellSpacing="0" cellPadding="0" width="100%">
+              <tbody>
+                <tr>
+                  <td
+                    id="Meta48576099ecdd41387849fbe603_Container"
+                    className={styles.tableDetail}
+                    width="100%"
+                  >
+                    <textarea
+                      name="Meta48576099ecdd41387849fbe603"
+                      rows={10}
+                      className={`${styles.w100} ${styles.textarea}`}
+                    ></textarea>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const NoExtensionTemplate = () => (
+  <div
+    style={{
+      width: "90%",
+      margin: "64px 5%",
+    }}
+  >
+    <SFImgixProductsSection />
+  </div>
+);
+
+export const NoExtension = NoExtensionTemplate.bind({});
+
+const WithExtensionTemplate = () => {
+  React.useLayoutEffect(() => {
+    injectExtensionApp();
+  }, []);
+  return <NoExtensionTemplate />;
+};
+export const WithExtension = WithExtensionTemplate.bind({});
+
+export default {
+  title: "Extension/Products Imgix Section",
+  component: SFImgixProductsSection,
+} as ComponentMeta<typeof SFImgixProductsSection>;
