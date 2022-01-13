2974d20482c696fd380c54a57c25200838ed0b63
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-01-13 16:52:01 +0100

    chore: add extension to bundle index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import { createBreakoutApp } from "./index-breakout";
 import { createSidebarApp } from "./index-sidebar";
+import { runExtension } from "./index-extension";
 
 if (process.env.REACT_APP_APP_TYPE === "sidebar") {
   createSidebarApp();
 } else if (process.env.REACT_APP_APP_TYPE === "breakout") {
   createBreakoutApp();
+} else if (process.env.REACT_APP_APP_TYPE === "extension") {
+  runExtension();
 } else {
   throw new Error("REACT_APP_APP_TYPE is not set");
 }
