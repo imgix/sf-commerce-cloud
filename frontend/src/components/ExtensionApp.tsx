d653003639d25b9d610bce4532445a254e87526b
Luis Ball <lball@imgix.com>
2022-01-14 11:53:01 -0500

    feat: create extension react app

---
+++ frontend/src/components/ExtensionApp.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/App.css";
+import { IImgixCustomAttributeValue } from "../types/imgixSF";
+import { ProductPageImages } from "./ProductPageImages";
+
+export type ISidebarAppProps = {
+  onChange: (value: string) => void;
+  onClear: () => void;
+  images: IImgixCustomAttributeValue[] | undefined;
+};
+
+export function App({ onChange, onClear, images }: ISidebarAppProps) {
+  const disabled = images === undefined || images.length === 0;
+  const onOpenBreakoutClick = () => {
+    console.log("[imgix] onOpenBreakoutClick");
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <ProductPageImages
+            onClick={onOpenBreakoutClick}
+            // TODO: add an `onClear` handler
+            images={images}
+            disabled={disabled}
+          />
+        </div>
+      </header>
+    </div>
+  );
+}
