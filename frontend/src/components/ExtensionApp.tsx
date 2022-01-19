48df3ec48d7c30bec53dea72d1ffade50ae5f28b
Luis Ball <lball@imgix.com>
2022-01-19 15:10:07 -0800

    feat: open im modal on click

--- frontend/src/components/ExtensionApp.tsx
+++ frontend/src/components/ExtensionApp.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { App as BreakoutApp } from "../components/BreakoutApp";
 import "../styles/App.css";
+import "../styles/index.css";
 import { IImgixCustomAttributeValue } from "../types/imgixSF";
+import Modal from "./layouts/Modal";
 import { ProductPageImages } from "./ProductPageImages";
 
 export type ISidebarAppProps = {
@@ -10,15 +13,36 @@ export type ISidebarAppProps = {
 
 export function ExtensionApp({ onChange, images }: ISidebarAppProps) {
   const disabled = images === undefined || images.length === 0;
-  const onOpenBreakoutClick = () => {
-    console.log("[imgix] onOpenBreakoutClick");
+  const [open, setOpen] = React.useState(false);
+  const [selectedAsset, setSelectedAsset] = React.useState(null);
+  const onModalClose = () => {
+    console.log("[imgix] Modal closed");
+    setOpen(false);
   };
+  const openModal = (data: any) => {
+    console.log("[imgix] Modal opened");
+    setSelectedAsset(data);
+    setOpen(true);
+  };
+
+  console.log("[imgix] images: ", images, selectedAsset);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <div className="ix-chrome-extension">
+          <Modal open={open} onClose={onModalClose} locked={false}>
+            <div className="modal-content">
+              <BreakoutApp
+                apiKey={""}
+                onSubmit={(data: IImgixCustomAttributeValue) => {
+                  onChange(JSON.stringify(data));
+                }}
+              />
+            </div>
+          </Modal>
           <ProductPageImages
-            onClick={onOpenBreakoutClick}
+            onClick={openModal}
             images={images}
             disabled={disabled}
           />
