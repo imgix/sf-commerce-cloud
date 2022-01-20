75343fe1580e6033f974cc73b2956ac6f87e14d9
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-01-20 13:12:35 +0100

    chore: refactor product images model to make clearer distinction between different modes of operation

--- cartridges/int_imgix_products_sfra/cartridge/models/product/productImages.js
+++ cartridges/int_imgix_products_sfra/cartridge/models/product/productImages.js
@@ -19,25 +19,105 @@ function Images(product, imageConfig) {
   const imgixDefaultParams =
     currentSite.getCustomPreferenceValue("imgixProductDefaultParams") || "";
 
-  if (isBaseURLSet) {
-    // Here we transform the URL
-    let imgixJsonImages = null;
-
+  const imgixCustomAttributeData = (() => {
     if (product instanceof ProductVariationModel) {
       // Get imgix data custom attribute from selected variant or master product
       const productData = product.selectedVariant || product.master;
-      imgixJsonImages =
+      return (
         productData &&
         productData.custom &&
         productData.custom.imgixData &&
-        JSON.parse(productData.custom.imgixData);
+        JSON.parse(productData.custom.imgixData)
+      );
     } else if (product instanceof Variant || product instanceof Product) {
-      imgixJsonImages =
+      return (
         product.custom &&
         product.custom.imgixData &&
-        JSON.parse(product.custom.imgixData);
+        JSON.parse(product.custom.imgixData)
+      );
     }
+    return undefined;
+  })();
 
+  /* The next section operates in one of three modes:
+   * 1. if custom attribute data exists, use that to render the images (as this
+   *    overrides the built-in SF data)
+   * 2. otherwise, if the imgixBaseURL is set, use that to render the images
+   * 3. fallback to the built-in SF data
+   */
+
+  if (imgixCustomAttributeData) {
+    imageConfig.types.forEach(function (type) {
+      var images = product.getImages(type);
+      var result = {};
+
+      if (imageConfig.quantity === "single") {
+        var firstImage = collections.first(images);
+        if (firstImage) {
+          let imageUrl =
+            imgixCustomAttributeData.images.primary.src +
+            (imgixDefaultParams ? "?" + imgixDefaultParams : "");
+          // TODO: update sourceWidth adding
+          imageUrl = appendSourceWidth(
+            imageUrl,
+            imgixCustomAttributeData.images.primary.sourceWidth
+          );
+
+          result = [
+            {
+              alt: firstImage.alt,
+              url: imageUrl,
+              title: firstImage.title,
+              index: "0",
+              absURL: imageUrl,
+            },
+          ];
+        }
+      } else {
+        result = collections.map(images, function (image, index) {
+          let imageUrl;
+          if (index === 0) {
+            // TODO: add tests for default params
+            imageUrl =
+              imgixCustomAttributeData.images.primary.src +
+              (imgixDefaultParams ? "?" + imgixDefaultParams : "");
+            // TODO: update sourceWidth adding
+            imageUrl = appendSourceWidth(
+              imageUrl,
+              imgixCustomAttributeData.images.primary.sourceWidth
+            );
+          } else {
+            const imageIndex = index - 1;
+            if (
+              imageIndex < imgixCustomAttributeData.images.alternatives.length
+            ) {
+              // TODO: add tests for default params
+              imageUrl =
+                imgixCustomAttributeData.images.alternatives[imageIndex].src +
+                (isBaseURLSet && imgixDefaultParams
+                  ? "?" + imgixDefaultParams
+                  : "");
+              // TODO: update sourceWidth adding
+              imageUrl = appendSourceWidth(
+                imageUrl,
+                imgixCustomAttributeData.images.alternatives[imageIndex]
+                  .sourceWidth
+              );
+            }
+          }
+
+          return {
+            alt: image.alt,
+            url: imageUrl,
+            index: index.toString(),
+            title: image.title,
+            absURL: imageUrl,
+          };
+        });
+      }
+      this[type] = result;
+    }, this);
+  } else if (isBaseURLSet) {
     imageConfig.types.forEach(function (type) {
       var images = product.getImages(type);
       var result = {};
@@ -51,17 +131,6 @@ function Images(product, imageConfig) {
             (isBaseURLSet && imgixDefaultParams
               ? "?" + imgixDefaultParams
               : "");
-          if (imgixJsonImages) {
-            // TODO: add tests for default params
-            imageUrl =
-              imgixJsonImages.images.primary.src +
-              (imgixDefaultParams ? "?" + imgixDefaultParams : "");
-            // TODO: update sourceWidth adding
-            imageUrl = appendSourceWidth(
-              imageUrl,
-              imgixJsonImages.images.primary.sourceWidth
-            );
-          }
 
           result = [
             {
@@ -69,7 +138,7 @@ function Images(product, imageConfig) {
               url: imageUrl,
               title: firstImage.title,
               index: "0",
-              absURL: firstImage.absURL.toString(),
+              absURL: imageUrl,
             },
           ];
         }
@@ -81,43 +150,13 @@ function Images(product, imageConfig) {
             (isBaseURLSet && imgixDefaultParams
               ? "?" + imgixDefaultParams
               : "");
-          if (imgixJsonImages) {
-            if (index === 0) {
-              // TODO: add tests for default params
-              imageUrl =
-                imgixJsonImages.images.primary.src +
-                (isBaseURLSet && imgixDefaultParams
-                  ? "?" + imgixDefaultParams
-                  : "");
-              // TODO: update sourceWidth adding
-              imageUrl = appendSourceWidth(
-                imageUrl,
-                imgixJsonImages.images.primary.sourceWidth
-              );
-            } else {
-              const imageIndex = index - 1;
-              if (imageIndex < imgixJsonImages.images.alternatives.length) {
-                // TODO: add tests for default params
-                imageUrl =
-                  imgixJsonImages.images.alternatives[imageIndex].src +
-                  (isBaseURLSet && imgixDefaultParams
-                    ? "?" + imgixDefaultParams
-                    : "");
-                // TODO: update sourceWidth adding
-                imageUrl = appendSourceWidth(
-                  imageUrl,
-                  imgixJsonImages.images.alternatives[imageIndex].sourceWidth
-                );
-              }
-            }
-          }
 
           return {
             alt: image.alt,
             url: imageUrl,
             index: index.toString(),
             title: image.title,
-            absURL: image.absURL.toString(),
+            absURL: imageUrl,
           };
         });
       }
@@ -146,7 +185,7 @@ function Images(product, imageConfig) {
         result = collections.map(images, function (image, index) {
           return {
             alt: image.alt,
-            url: firstImage.URL.toString(),
+            url: image.URL.toString(),
             index: index.toString(),
             title: image.title,
             absURL: image.absURL.toString(),
