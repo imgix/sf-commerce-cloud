02339c6ab2fce31478b5b36051045060d302a429
Luis Ball <lball@imgix.com>
2022-01-20 17:44:59 -0800

    chore: add container styles to add svg

--- frontend/src/components/icons/AddSvg.tsx
+++ frontend/src/components/icons/AddSvg.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "./Icon.module.scss";
 
 export const AddSvg = () => {
   return (
-    <svg viewBox="0 0 16 16">
-      <polygon points="15,6 10,6 10,1 6,1 6,6 1,6 1,10 6,10 6,15 10,15 10,10 15,10 " />
-    </svg>
+    <div className={styles.container}>
+      <svg viewBox="0 0 16 16">
+        <polygon points="15,6 10,6 10,1 6,1 6,6 1,6 1,10 6,10 6,15 10,15 10,10 15,10 " />
+      </svg>
+    </div>
   );
 };
