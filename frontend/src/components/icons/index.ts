68465af5b494ca24a8bfa409782232df8aedcf51
Luis Ball <lball@imgix.com>
2022-01-21 10:22:29 -0800

    chore: add icons index export

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,16 @@
+// export all the icons in this file
+export { AddSvg } from "./AddSvg";
+export { ArrowLeft } from "./ArrowLeft";
+export { ArrowRight } from "./ArrowRight";
+export { AzureIcon } from "./AzureIcon";
+export { DisabledSvg } from "./DisabledSvg";
+export { DownArrowSvg } from "./DownArrowSvg";
+export { GCSIcon } from "./GCSIcon";
+export { RefreshSvg } from "./RefreshSvg";
+export { S3Icon } from "./S3Icon";
+export { SearchIconSvg } from "./SearchIconSvg";
+export { SourceMenuSvg } from "./SourceMenuSvg";
+export { SourceTypeIcon } from "./SourceTypeIcon";
+export { TrashcanSvg } from "./TrashcanSvg";
+export { WebFolderIcon } from "./WebFolderIcon";
+export { WebProxyIcon } from "./WebProxyIcon";
