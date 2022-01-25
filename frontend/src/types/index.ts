56e3e6274c042aa611c9c561a8b89a7a9e729ba2
Luis Ball <lball@imgix.com>
2022-01-25 13:30:33 -0800

    chore: update type exports

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,3 +1,5 @@
-export type { ImgixGETSourcesData, ImgixGETAssetsData } from "./imgixAPITypes";
+export type { IBreakoutAppOnSubmit } from "./breakoutAppPublic";
 export type { CursorT } from "./cursor";
-export type { SandboxSubscribe } from "./sfcc";
+export type { ImgixGETAssetsData, ImgixGETSourcesData } from "./imgixAPITypes";
+export type { IImgixCustomAttributeValue } from "./imgixSF";
+export type { SandboxContext, SandboxSubscribe } from "./sfcc";
