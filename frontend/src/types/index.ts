bdbb834af4abadcb419dd41a6d4135cd86a07620
Luis Ball <lball@imgix.com>
2022-01-25 13:37:41 -0800

    chore: create product-image type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,4 +2,10 @@ export type { IBreakoutAppOnSubmit } from "./breakoutAppPublic";
 export type { CursorT } from "./cursor";
 export type { ImgixGETAssetsData, ImgixGETSourcesData } from "./imgixAPITypes";
 export type { IImgixCustomAttributeValue } from "./imgixSF";
+export type {
+  IImgixMetadata,
+  IProductImage,
+  IProductImageData,
+  ISwatchImages,
+} from "./productImage";
 export type { SandboxContext, SandboxSubscribe } from "./sfcc";
