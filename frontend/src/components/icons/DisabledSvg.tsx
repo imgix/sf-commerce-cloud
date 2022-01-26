ce3dfeab8a25caeba4a35a0d2bdb9f5aeae9ec05
Luis Ball <lball@imgix.com>
2022-01-26 15:53:54 -0800

    feat: add overrides to disabled svg

--- frontend/src/components/icons/DisabledSvg.tsx
+++ frontend/src/components/icons/DisabledSvg.tsx
@@ -1,8 +1,9 @@
 import styles from "./Icon.module.scss";
 
-export const DisabledSvg = () => {
+export const DisabledSvg = ({ className }: { className?: string }) => {
+  const classNames = [styles.container, className].join(" ");
   return (
-    <div className={styles.container}>
+    <div className={classNames}>
       <svg
         width="16"
         height="16"
@@ -10,7 +11,7 @@ export const DisabledSvg = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g opacity="0.5">
+        <g>
           <path
             d="M8 0C3.582 0 0 3.582 0 8C0 12.418 3.582 16 8 16C12.418 16 16 12.418 16 8C16 3.582 12.418 0 8 0ZM2 8C2 6.405 2.627 4.958 3.643 3.884L10.574 13.414C9.793 13.786 8.922 14 8 14C4.686 14 2 11.314 2 8ZM12.357 12.116L5.426 2.586C6.207 2.214 7.078 2 8 2C11.314 2 14 4.686 14 8C14 9.596 13.373 11.042 12.357 12.116Z"
             fill="white"
