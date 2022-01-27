3656acff9431f1e4ac39171bced5937b83c32d95
Luis Ball <lball@imgix.com>
2022-01-27 09:26:44 -0800

    feat!: update icons to all use icon-module

--- frontend/src/components/icons/DownArrowSvg.tsx
+++ frontend/src/components/icons/DownArrowSvg.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import styles from "./Icon.module.scss";
 
-export const DownArrowSvg = () => {
-  return (
-    <div className={styles.container}>
-      <svg viewBox="0 0 10 7" style={{ width: 10 }}>
-        <path d="M5 6.4L.3 1.7 1.7.3 5 3.6 8.3.3l1.4 1.4z"></path>
-      </svg>
-    </div>
-  );
-};
+export const DownArrowSvg = ({ className }: { className?: string }) => (
+  <div className={`${styles.container} ${className}`}>
+    <svg viewBox="0 0 10 7" style={{ width: 10 }}>
+      <path d="M5 6.4L.3 1.7 1.7.3 5 3.6 8.3.3l1.4 1.4z"></path>
+    </svg>
+  </div>
+);
