ab9149281bf68ba7ee07da126eb99cab8a5afe3c
Luis Ball <lball@imgix.com>
2022-01-27 09:26:44 -0800

    feat!: update icons to all use icon-module

--- frontend/src/components/icons/ArrowLeft.tsx
+++ frontend/src/components/icons/ArrowLeft.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Icon.module.scss";
 
-export const ArrowLeft = () => (
-  <div className={styles.container}>
+export const ArrowLeft = ({ className }: { className?: string }) => (
+  <div className={`${styles.container} ${className}`}>
     <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
       <path d="M14 5H9V2L1 8L9 14V11H14V5Z" />
     </svg>
