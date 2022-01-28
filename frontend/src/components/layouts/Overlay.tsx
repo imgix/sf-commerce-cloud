409a511639b256238ca2f5e13962aa5744dcc005
Luis Ball <lball@imgix.com>
2022-01-28 15:23:39 -0800

    feat: add rounded prop to overlay component

--- frontend/src/components/layouts/Overlay.tsx
+++ frontend/src/components/layouts/Overlay.tsx
@@ -4,8 +4,19 @@ import styles from "./Overlay.module.scss";
 interface Props {
   children: ReactChild | ReactChild[];
   visible: boolean;
+  rounded?: boolean;
 }
 
-export const Overlay = ({ children, visible }: Props): ReactElement => {
-  return visible ? <div className={styles.Overlay}>{children}</div> : <></>;
+export const Overlay = ({
+  children,
+  visible,
+  rounded,
+}: Props): ReactElement => {
+  return visible ? (
+    <div className={`${styles.Overlay} ${rounded && styles.rounded}`}>
+      {children}
+    </div>
+  ) : (
+    <></>
+  );
 };
