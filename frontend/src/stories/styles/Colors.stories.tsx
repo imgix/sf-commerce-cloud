e007984648de6a45c3f70d0fcba39693a223d99e
Luis Ball <lball@imgix.com>
2022-01-28 15:20:40 -0800

    test: organze colors
    This commit organizes the colors story to include hex codes and display
    the colors in a more logical sequence.

--- frontend/src/stories/styles/Colors.stories.tsx
+++ frontend/src/stories/styles/Colors.stories.tsx
@@ -3,6 +3,31 @@ import React from "react";
 import styles from "../../styles/Colors.module.scss";
 import { MultiStory } from "../common/MultiStory";
 
+// Helpers to convert color names to hex values
+function decToHex(value: number) {
+  if (value > 255) {
+    return "FF";
+  } else if (value < 0) {
+    return "00";
+  } else {
+    return value.toString(16).padStart(2, "0").toUpperCase();
+  }
+}
+function rgbToHex(r: number, g: number, b: number) {
+  return "#" + decToHex(r) + decToHex(g) + decToHex(b);
+}
+
+function rgbStringToHexString(rgbString: string) {
+  const rgb = rgbString.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  if (!rgb) {
+    return rgbString;
+  }
+  const r = parseInt(rgb[1]);
+  const g = parseInt(rgb[2]);
+  const b = parseInt(rgb[3]);
+  return rgbToHex(r, g, b);
+}
+
 const ColorDiv = ({
   color,
   children,
@@ -10,7 +35,25 @@ const ColorDiv = ({
   color: string;
   children: React.ReactNode;
 }) => {
-  return <div className={color}>{children}</div>;
+  // get computer style color from the element on first render and store it to state
+  const [renderedColor, setColor] = React.useState("");
+  React.useEffect(() => {
+    const element = document.getElementsByClassName(color);
+    const elementStyle = window.getComputedStyle(element[0]);
+    const elementColor = elementStyle.getPropertyValue("background-color");
+
+    if (element) {
+      setColor(rgbStringToHexString(elementColor));
+    }
+  }, [color]);
+  return (
+    <div className={color}>
+      <p style={{ justifyContent: "center", backgroundColor: "white" }}>
+        {renderedColor}
+      </p>
+      {children}
+    </div>
+  );
 };
 
 export default {
@@ -22,18 +65,21 @@ export default {
 } as ComponentMeta<typeof ColorDiv>;
 
 // create a storiesArray from every color in styles/Colors.module.scss
-const colorKeys = Object.keys(styles).filter((key) => key.includes("color"));
+let colorKeys = Object.keys(styles).filter((key) => key.includes("color"));
 
-const stories = colorKeys.map((key) => ({
-  label: key.replace("color", "").replace(/_/g, " ").toUpperCase(),
-  story: (
-    <ColorDiv color={styles[key]}>
-      {<div style={{ width: 100, height: 100, margin: 25 }}></div>}
-    </ColorDiv>
-  ),
-}));
+const stories = colorKeys.map((key) => {
+  const label = key.replace("color", "").replace(/_/g, " ").toUpperCase();
+  const container = (
+    <div id={key} style={{ width: 100, height: 100, margin: 25 }}></div>
+  );
 
-const Template: ComponentStory<typeof ColorDiv> = (args) => (
+  return {
+    label,
+    story: <ColorDiv color={styles[key]}>{container}</ColorDiv>,
+  };
+});
+
+const Template: ComponentStory<typeof ColorDiv> = () => (
   <MultiStory
     style={{
       display: "flex",
@@ -46,5 +92,4 @@ const Template: ComponentStory<typeof ColorDiv> = (args) => (
   ></MultiStory>
 );
 
-export const Palette = Template.bind({});
-Palette.args = {};
+export const Colors = Template.bind({});
