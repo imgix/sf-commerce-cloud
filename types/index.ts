87475732396ee7f310c5d8009d08cfdd9e429483
Luis Ball <lball@imgix.com>
2022-01-28 15:39:23 -0800

    build: add root-dirs and export from root

---
+++ types/index.ts
@@ -0,0 +1,16 @@
+export type {
+  CursorT,
+  IBreakoutAppOnSubmit,
+  IImgixCustomAttributeValue,
+  ImgixGETAssetsData,
+  ImgixGETSourcesData,
+  SandboxContext,
+  SandboxSubscribe,
+} from "../frontend/src/types/";
+export type {
+  IImgixCustomAttribute,
+  IImgixCustomAttributeImage,
+  IImgixCustomAttributeSwatch,
+  IImgixMetadata,
+} from "../src/cartridges/commonTypes";
+export type { ProductImagesModelData } from "../src/cartridges/SFTypes";
