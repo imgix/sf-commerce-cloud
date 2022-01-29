7136e61ffa528a3e50c3d2d1066cc40594977b84
Luis Ball <lball@imgix.com>
2022-01-28 16:11:35 -0800

    chore: use new types in extension and asset-card

--- frontend/src/components/card/AssetCard.tsx
+++ frontend/src/components/card/AssetCard.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from "react";
-import { ImgixGETAssetsData } from "../../types";
+import { IImgixMetadata, ImgixGETAssetsData } from "../../types";
 import { AssetCardImage } from "../image/AssetCardImage";
 import styles from "./AssetCard.module.scss";
 
 interface AssetCardProps {
-  asset: ImgixGETAssetsData[0];
+  asset: ImgixGETAssetsData[0] | IImgixMetadata | undefined;
   domain: string;
   height?: string;
   layout?: "grid" | "list";
@@ -26,7 +26,7 @@ export function AssetCard({
 }: AssetCardProps): ReactElement {
   // Strip filename from asset.attributes.origin_path
   const filename =
-    asset.attributes.origin_path.split("/").pop()?.split("?")[0] || "";
+    asset?.attributes.origin_path.split("/").pop()?.split("?")[0] || "";
   const isImage = filename.match(
     /\.(avif|gif|jp2|jpg|jpeg|json|jxr|pjpg|png|png8|png32|webp|blurhash|svg|ai)$/
   );
@@ -35,12 +35,15 @@ export function AssetCard({
     styles.container,
     layout === "list" ? styles.list : styles.grid,
     !isImage ? styles.disabled : "",
-    selectedAssetId === asset.id ? styles.selected : "",
+    selectedAssetId === asset?.id ? styles.selected : "",
     noFilepath ? styles["h-200"] : "",
   ].join(" ");
 
   return (
-    <div className={containerStyles} onClick={() => onClick && onClick(asset)}>
+    <div
+      className={containerStyles}
+      onClick={() => onClick && asset && onClick(asset)}
+    >
       {isImage ? (
         <div style={{ height }} className={styles.image}>
           <AssetCardImage asset={asset} domain={domain} />
