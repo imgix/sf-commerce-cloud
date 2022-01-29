86d7c2a72d59c9e42dc9db0bb26db7ed335072b2
Luis Ball <lball@imgix.com>
2022-01-28 16:11:35 -0800

    chore: use new types in extension and asset-card

--- frontend/src/components/ExtensionApp.tsx
+++ frontend/src/components/ExtensionApp.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../styles/App.css";
-import { IImgixCustomAttributeValue } from "../types/imgixSF";
+import { IProductImageData } from "../types/";
 import { ProductPageImages } from "./ProductPageImages";
 
 export type ISidebarAppProps = {
   onChange: (value: string) => void;
-  images: IImgixCustomAttributeValue[] | undefined;
+  data: IProductImageData | undefined;
 };
 
-export function ExtensionApp({ onChange, images }: ISidebarAppProps) {
+export function ExtensionApp({ onChange, data }: ISidebarAppProps) {
+  const images = data?.images || [];
   const disabled = images === undefined || images.length === 0;
   const onOpenBreakoutClick = () => {
     console.log("[imgix] onOpenBreakoutClick");
