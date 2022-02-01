0bc360f5dda18519e43e2738d818c3c87194ab26
Luis Ball <lball@imgix.com>
2022-02-01 13:29:39 -0800

    chore: ensure classname not set if prop undefined

--- frontend/src/components/icons/ArrowLeft.tsx
+++ frontend/src/components/icons/ArrowLeft.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Icon.module.scss";
 
 export const ArrowLeft = ({ className }: { className?: string }) => (
-  <div className={`${styles.container} ${className}`}>
+  <div className={`${styles.container} ${className ? className : ""}`}>
     <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
       <path d="M14 5H9V2L1 8L9 14V11H14V5Z" />
     </svg>
--- frontend/src/components/icons/DownArrowSvg.tsx
+++ frontend/src/components/icons/DownArrowSvg.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Icon.module.scss";
 
 export const DownArrowSvg = ({ className }: { className?: string }) => (
-  <div className={`${styles.container} ${className}`}>
+  <div className={`${styles.container} ${className ? className : ""}`}>
     <svg viewBox="0 0 10 7" style={{ width: 10 }}>
       <path d="M5 6.4L.3 1.7 1.7.3 5 3.6 8.3.3l1.4 1.4z"></path>
     </svg>
