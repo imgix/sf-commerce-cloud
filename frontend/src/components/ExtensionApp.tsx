25e3ce91a0f061b1502f5e0172e0e045dad2809d
Luis Ball <lball@imgix.com>
2022-02-02 15:29:14 -0800

    feat(extension): open im-modal on click
    This commit has the ExtensionApp accept an `apiKey` string prop. It then
    passes that prop onto the `AssetBrowserContainer` inside a `Modal`
    component. The modal only opens once the user has clicked on one of the
    buttons inside the `ProductImageContainer`.

--- frontend/src/components/ExtensionApp.tsx
+++ frontend/src/components/ExtensionApp.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import "../styles/App.css";
 import { IProductImageData } from "../types/";
+import { AssetBrowserContainer } from "./AssetBrowser/AssetBrowserContainer";
+import { Modal } from "./layouts/Modal";
 import { ProductPageImages } from "./ProductPageImages";
 
 export type ISidebarAppProps = {
   onChange: (value: string) => void;
+  apiKey: string | null;
   data: IProductImageData | undefined;
 };
 
-export function ExtensionApp({ onChange, data }: ISidebarAppProps) {
+export function ExtensionApp({ onChange, apiKey, data }: ISidebarAppProps) {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
   const images = data?.images || [];
   const disabled = images === undefined || images.length === 0;
   const onOpenBreakoutClick = () => {
-    console.log("[imgix] onOpenBreakoutClick");
+    console.log("[imgix] open imgix modal");
+    setIsModalOpen(true);
   };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <Modal
+            locked={false}
+            onClose={() => {
+              setIsModalOpen(false);
+            }}
+            open={isModalOpen}
+          >
+            <AssetBrowserContainer apiKey={apiKey} />
+          </Modal>
           <ProductPageImages
             onClick={onOpenBreakoutClick}
             images={images}
