2d4cc8763b7a1b51bbcdb9ecd835f62a67680fba
Luis Ball <lball@imgix.com>
2022-02-03 14:29:47 -0800

    feat: save asset to product images on click save

--- frontend/src/components/ExtensionApp.tsx
+++ frontend/src/components/ExtensionApp.tsx
@@ -1,24 +1,78 @@
 import React from "react";
-import { IImgixCustomAttribute } from "../../../types";
+import {
+  IImgixCustomAttribute,
+  IImgixCustomAttributeImage,
+} from "../../../types";
 import "../styles/App.css";
+import { IBreakoutAppData } from "../types/breakoutAppPublic";
 import { AssetBrowserContainer } from "./AssetBrowser/AssetBrowserContainer";
+import { FrameButton } from "./buttons/FrameButton/FrameButton";
 import { Modal } from "./layouts/Modal";
 import { ProductPageImages } from "./ProductPageImages";
 
 export type ISidebarAppProps = {
-  onChange: (value: string) => void;
+  onChange: (data: IImgixCustomAttribute) => void;
   apiKey: string | null;
   data: IImgixCustomAttribute | undefined;
 };
 
 export function ExtensionApp({ onChange, apiKey, data }: ISidebarAppProps) {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
-  const images = data?.images || [];
-  const disabled = images === undefined || images.length === 0;
-  const onOpenBreakoutClick = () => {
-    console.log("[imgix] open imgix modal");
+  const [selectedProductImageId, setSelectedProductImageId] = React.useState(
+    ""
+  );
+  const [selectedAssetImage, setSelectedAssetImage] = React.useState<
+    IImgixCustomAttributeImage | undefined
+  >(undefined);
+
+  const openModal = () => {
+    console.info("[imgix] open imgix modal");
     setIsModalOpen(true);
   };
+
+  const onProductImageClick = (id: string) => {
+    console.log("[imgix] onProductImageClick id:", id);
+
+    setSelectedProductImageId(id);
+    openModal();
+  };
+
+  const onSelectAsset = (assetData: IBreakoutAppData) => {
+    const { src, imgix_metadata: metadata } = { ...assetData };
+
+    const newImage = {
+      src,
+      imgix_metadata: metadata,
+      // SFCC requires all images have a alt/title attributes
+      title: metadata.attributes.name || metadata.attributes.origin_path,
+      alt: metadata.attributes.name || metadata.attributes.origin_path,
+    };
+
+    setSelectedAssetImage(newImage);
+  };
+
+  const saveSelectionToDataOnClick = () => {
+    if (selectedProductImageId && selectedAssetImage) {
+      console.log("[imgix] selectedAssetImage exists, replacing data");
+      // for image with matching id, replace the image with the selected asset
+      const newImages = data?.images.map((image) => {
+        if (image.imgix_metadata?.id === selectedProductImageId) {
+          return selectedAssetImage;
+        }
+        return image;
+      });
+      onChange({ images: newImages || [] });
+    } else if (selectedAssetImage) {
+      console.log("[imgix] asset selected, adding to data");
+      const newImages = data?.images.concat(selectedAssetImage);
+      onChange({ images: newImages || [] });
+    } else {
+      console.log("[imgix] selectedAssetImage does not exist, not adding data");
+    }
+  };
+
+  const images = data?.images || [];
+  const disabled = images === undefined || images.length === 0;
 
   return (
     <div className="App">
@@ -31,10 +85,14 @@ export function ExtensionApp({ onChange, apiKey, data }: ISidebarAppProps) {
             }}
             open={isModalOpen}
           >
-            <AssetBrowserContainer apiKey={apiKey} />
+            <AssetBrowserContainer
+              apiKey={apiKey}
+              onSelectAsset={onSelectAsset}
+            />
+            <FrameButton label="save" onClick={saveSelectionToDataOnClick} />
           </Modal>
           <ProductPageImages
-            onClick={onOpenBreakoutClick}
+            onClick={onProductImageClick}
             images={images}
             disabled={disabled}
           />
