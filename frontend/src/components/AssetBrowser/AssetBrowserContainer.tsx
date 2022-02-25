28fba193b7f30075222f2191728e73605eb8e12a
Luis Ball <lball@imgix.com>
2022-02-24 18:00:04 -0800

    chore: remove unecessary return statement

--- frontend/src/components/AssetBrowser/AssetBrowserContainer.tsx
+++ frontend/src/components/AssetBrowser/AssetBrowserContainer.tsx
@@ -67,7 +67,6 @@ export class AssetBrowserContainer extends Component<Props, State> {
           );
           source && this.requestAssetsFromSource({ source });
         }
-        return resp.data;
       })
       .catch((err) => {
         this.setState({
