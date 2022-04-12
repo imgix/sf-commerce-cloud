2d01c8f1166ef5db233d00daa44c72ff1105c563
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-04-12 15:58:37 +0200

    chore: change plugin header depending on app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,16 @@
 import { createBreakoutApp } from "./index-breakout";
-import { createSidebarApp } from "./index-sidebar";
 import { runExtension } from "./index-extension";
+import { createSidebarApp } from "./index-sidebar";
+import { setAPIEnvironment } from "./services/imgixAPIService";
 
 if (process.env.REACT_APP_APP_TYPE === "sidebar") {
+  setAPIEnvironment("page_designer");
   createSidebarApp();
 } else if (process.env.REACT_APP_APP_TYPE === "breakout") {
+  setAPIEnvironment("page_designer");
   createBreakoutApp();
 } else if (process.env.REACT_APP_APP_TYPE === "extension") {
+  setAPIEnvironment("products");
   runExtension();
 } else {
   throw new Error("REACT_APP_APP_TYPE is not set");
